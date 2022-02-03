38739a7644684d885ef1d854a9c378be4f704c80
Javier de Jesús Pérez Santiago <javier.de.jesus.perez.santiago@gmail.com>
2022-02-03 17:27:57 -0600

    3 feb

---
+++ src/metodos/Metodos.java
@@ -0,0 +1,63 @@
+package metodos;
+//enlazado a PruebaMetodos
+public class Metodos {
+	//https://docs.oracle.com/javase/8/docs/api/
+	//Metodos son fuciones
+	//en vez de public puedes poner private para que la funcion solo sirva en la clase que se creo
+	//atributos de la clase
+	int a, b;
+	
+	/////////////////////Metodos void (Vacio)
+	//public void para ser utilizado en otra clase
+	//con void por que no regresa resultados
+	public void sumar() {
+		int resultado = a + b;
+		System.out.println("El resultado de sumar " + a + " mas " + b + " es " + resultado);
+	}
+	
+	
+	//////////////////////metodo con retorno de valores
+	//sin void pero especifica el tipo de valor
+	public int sumaConRetorno () {
+		//int resultado = a + b;
+		//return resultado;//regresame resultado de resultado
+		return a + b;//regresame a + b
+	}
+	
+	
+	
+	
+	////////////////////////Argumentos
+	//no lleva void , pero lleva el tipo de dato que manejara
+	//suma(tipoDeValor argumentos); los argumetnos pueden ser int y otro double; especifica que tipo de dato son
+	//double mas int da double, en operaciones esta bien mezclar pero no lo hagas en resultados,
+	//en este caso la funcion recogera un double y debe ser especificado.
+	//variables fuera vs argumentos: variables puede ser usada por cualquir funcion, argumentos son valores que creamos
+	//dentro de la funcion, para hacer operaciones sin depender de variables externas
+	public double multiplicarConArgumentos(int arg1, double arg2) {
+		int c = arg1;
+		double d = arg2;
+		return c * d;
+		//podria escribir solo return arg1 + arg2
+	}
+	
+	
+	
+	
+	
+	//////////////////Método estático
+	//no requiere la existencia de un objeto para existir
+	//estatic, void(no regresa datos), usa argumentos
+	public static void imprimirNombre(String nombre) {
+		System.out.println("Mi nombre es: " + nombre );
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ src/metodos/PruebaMetodos.java
@@ -0,0 +1,47 @@
+package metodos;
+
+//nueva clase que tiene acceo al metodo que creamos en Metodos
+public class PruebaMetodos {
+	public static void main (String[] args) {
+		
+		
+		//crea un objeto que va a contener los metodos(funciones) y atributos(variables)
+		//clase donde viene + nombre(nostros se lo damos) = + new + contenido de donde viene
+		Metodos metodoPrueba = new Metodos ();//creamos un objeto
+		
+		
+		
+		metodoPrueba.a = 10;//variable a de la clase Metodos
+		metodoPrueba.b = 26;//variable b de la clase Metodos
+		
+		
+		metodoPrueba.sumar();//invocar el metodo de la clase Metodos//metodo void
+		//en consola muestra el println que esta contenido en esa funcion
+		
+		
+		//este tipo de metodos se pueden almacenar dentro de una variable
+		int resultadoSuma = metodoPrueba.sumaConRetorno();//invocar metodo con retorno con una variable int
+		System.out.println(resultadoSuma - 10);//metodo con retorno
+		
+	
+		//metodo con argumentos
+		double multiplicacion = metodoPrueba.multiplicarConArgumentos(40, 20);//double por que recupera un double
+		System.out.println("El resultado de multiplicacion es: " + multiplicacion);
+		
+		
+		
+		
+		
+		Metodos.imprimirNombre("Pancho");//puede ser invocado sin un objeto linea 10{ Metodos metodoPrueba = new Metodos ();}
+		//la razon es que pertenece a la clase
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+} 
